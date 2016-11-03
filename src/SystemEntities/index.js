ff980c09c6cf72ef8e584e47d7eb8f8d0fb3685a
YuuKohaku <soul.of.bytes@gmail.com>
2016-11-03 17:17:02 +0700

    service name

--- src/SystemEntities/index.js
+++ src/SystemEntities/index.js
@@ -1,19 +1,20 @@
 'use strict'
 
 let events = {
-  system_entities: {}
+	system_entities: {}
 }
 
 let tasks = [];
 
 
 module.exports = {
-  module: require('./system-entities.js'),
-  permissions: [],
-  tasks: tasks,
-  exposed: true,
-  events: {
-    group: 'system-entities',
-    shorthands: events.system_entities
-  }
+	module: require('./system-entities.js'),
+	name: 'system-entities',
+	permissions: [],
+	tasks: tasks,
+	exposed: true,
+	events: {
+		group: 'system-entities',
+		shorthands: events.system_entities
+	}
 };
